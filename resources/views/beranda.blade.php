4d79c6573fd9df1677a559281854443a60c29b76
adit1a <ardimuh50@gmail.com>
2026-04-27 06:09:34 +0800

    Initial commit proyek Pramuka_Id

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, 
+    nitial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Beranda</title>
+</head>
+<body>
+    <h1>ggg</h1>
+    <main>ini main</main>
+    <footer>ini footer</footer>
+</body>
+</html>
