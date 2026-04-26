b5e558b4478acb423f4b74c83fc7c632b1a3185b
adit1a <ardimuh50@gmail.com>
2026-04-27 07:53:33 +0800

    Update Tampilan beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -8,8 +8,53 @@
     <title>Beranda</title>
 </head>
 <body>
-    <h1>ggg</h1>
-    <main>ini main</main>
-    <footer>ini footer</footer>
+    <nav class="navbar">
+        <div class="container">
+            <div class="logo">Logo</div>
+            <ul class="nav-menu">
+                <li><a href="/">Home</a></li>
+                <li><a href="/form">Form</a></li>
+                <li><a href="#">Achivement</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Log out</a></li>
+            </ul>
+        </div>
+    </nav>
+    <header class="hero">
+        <div class="hero-content">
+            <h1>Satu Pramuka, Untuk Satu Indonesia</h1>
+            <p>Mari kita bangun karakter pramuka yang kuat 
+                dan bermoral</p>
+            <a href="#" class="btn-hero">Klik Disini</a>
+        </div>
+    </header>
+    <main id="main">
+        <section class="headline">
+            <h2>Kreatif, Inovatif, dan Berkarakter</h2>
+            <div class="line"></div>
+        </section>
+        <section class="parallax">
+            <div class="parallax-text">Membangun Bangsa</div>
+        </section>
+        <section class="banner-area">
+            <div class="card-hover">
+                <div class="card-img">
+                <img src="https://images.unsplash.com/photo-1504221507732-5246c045949b?q=80&w=500" alt="Kegiatan">
+            </div>
+                <h3>Kegiatan Baru</h3>
+                <p>Cek Agenda Perkemahan Terbaru di Sini.</p>
+            </div>
+            <div class="card-hover">
+                <div class="card-img">
+                    <img src="https://images.unsplash.com/photo-1504221507732-5246c045949b?q=80&w=500" alt="Materi">
+                </div>
+                <h3>Materi Kepramukaan</h3>
+                <p>Pelajari SKU, SKK, dan Sandi-sandi Digital</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2026 Pramuka_Sulawesi_Tenggara. Dibuat dengan senang dan gembira tanpa paksaan</p>
+    </footer>
 </body>
 </html>
