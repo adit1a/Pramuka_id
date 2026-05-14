63e7883b15f00b580868fec638f8f61a74a613fa
adit1a <ardimuh50@gmail.com>
2026-05-14 10:26:15 +0800

    Solusi nuklir untuk read-only error

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
 <?php
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/bootstrap/cache',
+];
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
