752d22d2833fe7cf87de5fbc10e9ae20b3796b1e
adit1a <ardimuh50@gmail.com>
2026-05-15 08:42:54 +0800

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,8 +51,12 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+$app->setBootstrapCachePath('/tmp/storage/bootstrap/cache');
+
 if (env('APP_ENV') === 'production') {
     $app->useStoragePath('/tmp/storage');
 }
+
+$app->useStoragePath('/tmp/storage');
 
 return $app;
