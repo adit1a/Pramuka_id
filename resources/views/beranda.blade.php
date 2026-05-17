ac0a3f4fd8a5af728e493b6e41388b69dd608aab
adit1a <ardimuh50@gmail.com>
2026-05-17 20:34:23 +0800

    maintanance 4

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -79,7 +79,7 @@
             <div class="container px-3 px-md-4">
                 <div class="row align-items-center">
                     <div class="col-lg-4 col-md-5 mb-4 mb-md-0 text-center" data-aos="fade-right">
-                        <img src="{{ secure_asset('img/ketua.png') }}" alt="Ketua Kwarcab Kolaka Utara" class="img-fluid rounded-4 shadow foto-ketua">
+                        <img src="{{ secure_asset('img/ketua.jpeg') }}" alt="Ketua Kwarcab Kolaka Utara" class="img-fluid rounded-4 shadow foto-ketua">
                     </div>
 
                     <div class="col-lg-8 col-md-7" data-aos="fade-left">
